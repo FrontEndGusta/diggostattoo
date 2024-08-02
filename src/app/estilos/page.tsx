489febdb76ad0e55a26e04a2873073f37348681f
Gustavo Silva De Lima <gustavo.lima@cea.com.br>
2024-08-02 09:13:24 -0300

    adicionado animações e corrigido type de estilos 4

--- src/app/estilos/page.tsx
+++ src/app/estilos/page.tsx
@@ -4,7 +4,7 @@ import image1 from "@/assets/teste.png";
 import useAosAnimation from "@/components/hooks/useWindowWidth";
 import React, { forwardRef } from "react";
 
-export type EstilosProps = {
+export interface EstilosProps {
   estilosRef: React.RefObject<HTMLDivElement>;
 };
 
