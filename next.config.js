6a71addb18a834a10f1934ca96a378daa498205c
Gustavo Silva De Lima <gustavo.lima@cea.com.br>
2024-08-02 11:35:00 -0300

    ajustando imagens

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ module.exports = {
       domains: ['images.unsplash.com', 'plus.unsplash.com', 'img.freepik.com', 'media.istockphoto.com'],
     },
     typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
       ignoreBuildErrors: true,
     },
   }
--- src/components/3DCardEffect/3DCardEffect.tsx
+++ src/components/3DCardEffect/3DCardEffect.tsx
@@ -12,11 +12,17 @@ const imageUrls = [
   "https://images.unsplash.com/photo-1520880867055-1e30d1cb001c?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1520880867055-1e30d1cb001c?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1520880867055-1e30d1cb001c?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1520880867055-1e30d1cb001c?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1520880867055-1e30d1cb001c?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1520880867055-1e30d1cb001c?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1520880867055-1e30d1cb001c?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1520880867055-1e30d1cb001c?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ];
 
 export function ThreeDCardDemo() {
   return (
-    <div className="flex flex-wrap justify-center gap-6">
+    <div className="flex flex-wrap justify-between gap-6">
       {imageUrls.map((src, index) => (
         <div key={index} data-aos="zoom-out-up">
           <CardContainer className="inter-var">
@@ -25,7 +31,7 @@ export function ThreeDCardDemo() {
                 src={src}
                 height="1000"
                 width="1000"
-                className="h-60 w-full object-cover group-hover/card:shadow-xl"
+                className="min-h-[300px] h-[300px] w-full lg:w-[270px] lg:h-[250px] object-cover group-hover/card:shadow-xl"
                 alt={`thumbnail-${index}`}
               />
             </CardItem>
